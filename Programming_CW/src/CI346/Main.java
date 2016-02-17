fc1f3956667c4e45961bb748102ac56d5c97b037
Michael <mikepriest123@yahoo.co.uk>
2016-02-17 13:43:10 +0000

    Completed the concurrency's
    Completed no, explicit and inplicit

--- Programming_CW/src/CI346/Main.java
+++ Programming_CW/src/CI346/Main.java
@@ -1,25 +1,55 @@
 package CI346;
 
+
+import java.util.stream.Stream;
+
 public class Main {
-	public static void main(String[] args) 
+	
+	public static void main(String[] args) throws InterruptedException 
 	{
-		int primeValue = 250000;
-		noConcurrency.noConcurrencyPrime(primeValue);
-		Thread t1 = new Thread(new explicitConcurrency(2, primeValue/2));
-		Thread t2 = new Thread(new explicitConcurrency(primeValue/2, primeValue));
-		long startTimer = System.nanoTime();
+		int primeStart = 2;
+		int primeEnd = 100000;
+		long startTimer = 0;
+		long endTimer = 0;
+		
+		System.out.printf("The range of numbers is between %d and %d\n\n", primeStart, primeEnd);
+		
+		
+		//NO CONCURRENCY
+		
+		startTimer = System.nanoTime();
+		noConcurrency.noConcurrencyPrime(primeStart, primeEnd);
+		endTimer = System.nanoTime();
+		System.out.printf("The no concurrency program took %s seconds to complete and found %d prime numbers\n\n\n", calculateTimeTaken(startTimer, endTimer), noConcurrency.getCount());
+		
+		
+		//EXPLICIT CONCURRENCY
+		
+		Thread t1 = new Thread(new explicitConcurrency(primeStart, primeEnd/2));
+		Thread t2 = new Thread(new explicitConcurrency(primeEnd/2, primeEnd));
+		startTimer = System.nanoTime();
 		t1.start();	
 		t2.start();
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			
-		}
-		long stopTimer = System.nanoTime();
-		double tt = (double) (stopTimer - startTimer) / 1000000000;
-		System.out.printf("The program took %.5f seconds to execute\n\n\n", tt);
-		System.out.println("Value: " + explicitConcurrency.getCount());
+		t1.join();
+		t2.join();
+		endTimer = System.nanoTime();
+		System.out.printf("The explicit concurrency program took %s seconds to complete and found %d prime numbers\n\n\n", calculateTimeTaken(startTimer, endTimer), explicitConcurrency.getCount());
+		
+		
+		//IMPLICIT CONCURRENCY
+		
+		startTimer = System.nanoTime();
+		long count = Stream.iterate(primeStart , a -> a+1).parallel().limit(primeEnd - primeStart)
+				.filter(b -> implicitConcurrency.calculatePrimes(b)).count();
+		endTimer = System.nanoTime();
+		System.out.printf("The implicit concurrency program took %s seconds to complete and found %d prime numbers\n\n\n", calculateTimeTaken(startTimer, endTimer), count);	
 	}
+	
+	public static String calculateTimeTaken(long startTime, long endTime)
+	{
+		double timeTaken = (double) (endTime - startTime) / 1000000000;
+		return String.format("%.5f", timeTaken);
+	}
+	
 }
 
--- Programming_CW/src/CI346/noConcurrency.java
+++ Programming_CW/src/CI346/noConcurrency.java
@@ -1,21 +1,23 @@
 package CI346;
 
 public class noConcurrency {
-	public static void noConcurrencyPrime(int primeValue) {
-		int primeCount = 0;
-		long startTimer = System.nanoTime();
-		for (int i = 2; i < primeValue; i++) {
+	
+	static int primeCount = 0;
+	
+	public static void noConcurrencyPrime(int primeStart, int primeEnd) 
+	{
+		for (int a = primeStart; a < primeEnd; a++) {
 			boolean isPrimeNumber = true;
-			if (i % 2 == 0) 
+			if (a % 2 == 0) 
 			{
-				if (i == 2) 
+				if (a == 2) 
 				{
 					primeCount++;
 				}
 			} else {
-				for (int j = 3; j < i; j += 2) 
+				for (int b = 3; b < a; b += 2) 
 				{
-					if (i % j == 0) 
+					if (a % b == 0) 
 					{
 						isPrimeNumber = false;
 						break;
@@ -27,10 +29,11 @@ public class noConcurrency {
 				}
 			}
 		}
-		long stopTimer = System.nanoTime();
-		double tt = (double) (stopTimer - startTimer) / 1000000000;
-		System.out.println("There are " + primeCount
-				+ " prime numbers between 1 and " + primeValue);
-		System.out.printf("The program took %.5f seconds to execute\n\n\n", tt);
 	}
+	
+	public static int getCount()
+	{
+		return primeCount;
+	}
+	
 }
